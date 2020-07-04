ba2354ea51024afc48f93bffe3e8c5e4fb630487
MylesRC <myles_chan@dlsu.edu.ph>
2020-07-04 23:51:08 +0800

    start up server

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const moment = require('moment');
 const fs = require('fs');
+
+router.get("/", async (req, res) => {
+})
 
 module.exports = router;
