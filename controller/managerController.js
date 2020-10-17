1cc765764eb121390a75cbdcb8b3d075a7021cd3
ShyreneChing <shyrene_ching@dlsu.edu.ph>
2020-10-18 01:01:36 +0800

    created edit instance functions

--- controller/managerController.js
+++ controller/managerController.js
@@ -451,6 +451,193 @@ router.post("/editBookInstance", urlencoder, async (req, res) => {
     res.redirect("/book?data_id=" + book._id)
 })
 
+router.post("/editBookInstanceReserved", urlencoder, async (req, res) => {
+    console.log("req.session.username " + req.session.username)
+    console.log("req.body.editbookinstance_id " + req.body.editbookinstance_id)
+    let userID = req.session.username;
+    let instanceID = req.body.editbookinstance_id;
+    let borrowerID = req.body.borrowerID;
+    
+    let instance = await BookInstance.getBookInstanceByID(instanceID)
+    let book = await Book.getBookByID(instance.book);
+    let datetime = moment().format('YYYY-MM-DD HH:mm')
+
+    let date_available = moment().add(14, 'days').format('YYYY-MM-DD HH:mm')
+    let actual_returned = '';
+    let status = 'borrowed';
+    
+    temp = await Author.getAuthorByID(book.author[0]);
+    let authorDisplay = temp.firstname + " " + temp.lastname
+    for (var i = 1; i < book.author.length; i++) {
+        temp = await Author.getAuthorByID(book.author[i]);
+        authorDisplay = authorDisplay + ", " + temp.firstname + " " + temp.lastname
+    }
+    
+    let user = await User.getUserByID(userID);
+    let borrower = await User.getUserByID(borrowerID);
+    let item = book.title + " by " + authorDisplay
+    let action = 'Successfully borrowed book for ' + borrower.username;
+
+    let borrowHistory = new BorrowHistory({
+        book: instanceID,
+        author: book.author,
+        user: borrowerID,
+        time_barrow: moment().format('YYYY-MM-DD HH:mm'),
+        due_date: date_available,
+        actual_returned,
+        status
+    });
+    
+    BorrowHistory.addBarrowHistory(borrowHistory, async function (borrowHistory) {
+        if (borrowHistory) {
+            let sysLogs = new SystemLogs({
+                action,
+                actor: user.username,
+                ip_add: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+                        req.connection.remoteAddress || 
+                        req.socket.remoteAddress || 
+                        req.connection.socket.remoteAddress,
+                item,
+                datetime: moment().format('YYYY-MM-DD HH:mm')
+            });
+            SystemLogs.addLogs(sysLogs);
+        
+            await BookInstance.updateInstance(instanceID, "Reserved", date_available)
+
+            res.redirect("/book?data_id=" + book._id)
+        } else {
+            let syslog = new SystemLogs({
+                action: "Failed to Borrow Book",
+                actor: user.username,
+                ip_add: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+                    req.connection.remoteAddress || 
+                    req.socket.remoteAddress || 
+                    req.connection.socket.remoteAddress,
+                item: item,
+                datetime: moment().format('YYYY-MM-DD HH:mm')
+            })
+            SystemLogs.addLogs(syslog)
+
+            console.log("Failed to Borrow Book")
+            res.redirect("/error");
+        }
+    }, (error) => {
+        let syslog = new SystemLogs({
+            action: "Error",
+            actor: null,
+            ip_add: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+                req.connection.remoteAddress || 
+                req.socket.remoteAddress || 
+                req.connection.socket.remoteAddress,
+            item: error.message,
+            datetime: moment().format('YYYY-MM-DD HH:mm')
+        })
+        SystemLogs.addLogs(syslog)
+
+        res.redirect("/error");
+    })
+    // let sysLogs = new SystemLogs({
+    //     action,
+    //     actor: user.username,
+    //     ip_add: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+    //             req.connection.remoteAddress || 
+    //             req.socket.remoteAddress || 
+    //             req.connection.socket.remoteAddress,
+    //     item,
+    //     datetime
+    // });
+    // SystemLogs.addLogs(sysLogs);
+
+    // let newInstance = await BookInstance.updateInstance(instanceID, status, date_available);
+
+    // res.redirect("/book?data_id=" + book._id)
+})
+
+router.post("/chageDueDateInstance", urlencoder, async (req, res) => {
+    console.log("req.session.username " + req.session.username)
+    console.log("req.body.editbookinstance_id " + req.body.editbookinstance_id)
+    let userID = req.session.username;
+    let instanceID = req.body.editbookinstance_id;
+    let status = "Available";
+    if(req.body.status == "status_reserved"){
+        status = "Reserved"
+    } 
+    
+    let date_available = moment(req.body.editbookinstance_dateavailable, 'MMMM DD, YYYY H:mm A').format('YYYY-MM-DD HH:mm')
+ 
+    let instance = await BookInstance.getBookInstanceByID(instanceID)
+    let book = await Book.getBookByID(instance.book);
+    let datetime = moment().format('YYYY-MM-DD HH:mm')
+    
+    temp = await Author.getAuthorByID(book.author[0]);
+    let authorDisplay = temp.firstname + " " + temp.lastname
+    for (var i = 1; i < book.author.length; i++) {
+        temp = await Author.getAuthorByID(book.author[i]);
+        authorDisplay = authorDisplay + ", " + temp.firstname + " " + temp.lastname
+    }
+    
+    let user = await User.getUserByID(userID);
+    let item = book.title + " by " + authorDisplay
+    let action = 'Successfully Edited Due Date of Book Instance';
+
+    let sysLogs = new SystemLogs({
+        action,
+        actor: user.username,
+        ip_add: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+                req.connection.remoteAddress || 
+                req.socket.remoteAddress || 
+                req.connection.socket.remoteAddress,
+        item,
+        datetime
+    });
+    SystemLogs.addLogs(sysLogs);
+
+    let newInstance = await BookInstance.updateInstance(instanceID, "Reserved", date_available);
+
+    res.redirect("/book?data_id=" + book._id)
+})
+
+router.post("/returnBookInstance", urlencoder, async (req, res) => {
+    console.log("req.session.username " + req.session.username)
+    console.log("req.body.editbookinstance_id " + req.body.editbookinstance_id)
+    let userID = req.session.username;
+    let instanceID = req.body.editbookinstance_id;
+    
+    let instance = await BookInstance.getBookInstanceByID(instanceID)
+    let book = await Book.getBookByID(instance.book);
+    let datetime = moment().format('YYYY-MM-DD HH:mm')
+    
+    temp = await Author.getAuthorByID(book.author[0]);
+    let authorDisplay = temp.firstname + " " + temp.lastname
+    for (var i = 1; i < book.author.length; i++) {
+        temp = await Author.getAuthorByID(book.author[i]);
+        authorDisplay = authorDisplay + ", " + temp.firstname + " " + temp.lastname
+    }
+    
+    let user = await User.getUserByID(userID);
+    let item = book.title + " by " + authorDisplay
+    let action = 'Successfully Edited a Book Instance';
+
+    let history =  await BorrowHistory.getUnreturnedBookHistory(instanceID)
+    let sysLogs = new SystemLogs({
+        action: "Returned a book for a user",
+        actor: user.username,
+        ip_add: (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
+                req.connection.remoteAddress || 
+                req.socket.remoteAddress || 
+                req.connection.socket.remoteAddress,
+        item,
+    })
+
+    SystemLogs.addLogs(sysLogs);
+
+    await BorrowHistory.updateTimeReturnedByID(history._id, moment().format('YYYY-MM-DD HH:mm'));
+
+    let newInstance = await BookInstance.updateInstance(instanceID, "Available", null);
+
+    res.redirect("/book?data_id=" + book._id)
+})
+
 router.post('/returnBook', function(req, res) {
     // console.log(req.body.data_id)
     Book.findOne({
